0192562a443be5fa7c2d3234d47470452332ed97
owigo <52714800+owiga@users.noreply.github.com>
2025-05-19 13:11:24 +0400

    First Update V1.0

--- main.py
+++ main.py
@@ -24,31 +24,33 @@ class Game:
         self.button.rect.center = (RESOLUTION[0] // 2, RESOLUTION[1] // 2)
 
         self.upgrades_buttons = []
-        self.first_upgrade = UpgradeButton(1, ID_UPGRADE_COST[str(1)][2])
-        self.first_upgrade.rect.x = 5
-        self.first_upgrade.rect.y = 10
-        self.first_upgrade_price_btn = self.font_income.render(f"Цена: {self.first_upgrade.get_price()}", True, (255, 255, 255))
-        self.upgrades_buttons.append(self.first_upgrade)
+        for i in range(0, 3):
+            button = UpgradeButton(i + 1, ID_UPGRADE_COST[str(i + 1)][2])
+            button.rect.x = 5
+            button.rect.y = 60 * i + 10
+            button_text = self.font_income.render(f"Цена: {button.get_price()}", True, (255, 255, 255))
+            self.upgrades_buttons.append((button, button_text, i))
 
         self.balance = self.font.render(f"Баланс: {self.money}", True, (255, 255, 255))
         self.earn = self.font_income.render(
             f"Вы получите: {self.base_income * self.multiplier * self.rebirth_multiplier}", True, (255, 255, 255))
-        self.prices = [[self.first_upgrade_price_btn,
-                        (BUTTON_SIZE[0] + self.first_upgrade_price_btn.get_rect().size[0] // 4, self.first_upgrade.rect.y)]]
+
 
         self.balance_trigger = pygame.USEREVENT + 1
         pygame.time.set_timer(self.balance_trigger, 100)
 
-        self.SpriteGroup = pygame.sprite.Group([self.button, self.first_upgrade])
+        self.SpriteGroup = pygame.sprite.Group([self.button, *[x[0] for x in self.upgrades_buttons]])
         pygame.display.set_caption("Clicker")
 
     def run(self):
         while True:
+            self.clock.tick(200)
             self.screen.fill(BACKGROUND_COLOR)
             self.screen.blit(self.balance, (RESOLUTION[0] // 2 - self.balance.get_rect().size[0] // 2, RESOLUTION[1] // 20))
             self.screen.blit(self.earn, (RESOLUTION[0] // 2 - self.earn.get_rect().size[0] // 2, RESOLUTION[1] // 11))
-            for text in self.prices:
-                self.screen.blit(text[0], text[1])
+            for button, text, index in self.upgrades_buttons:
+                text = self.font_income.render(f"Цена: {button.get_price()}", True, (255, 255, 255))
+                self.screen.blit(text, (RESOLUTION[0] // 6, 60 * index + 25))
             self.SpriteGroup.draw(self.screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -59,17 +61,20 @@ class Game:
                         self.money += self.base_income * self.multiplier * self.rebirth_multiplier
                     else:
                         for button in self.upgrades_buttons:
-                            if button.rect.collidepoint(event.pos):
-                                if self.money >= button.get_price():
-                                    self.money -= button.get_price()
-                                    if ID_UPGRADE_COST[str(button.get_id())][1] == "INC":
-                                        self.base_income += ID_UPGRADE_COST[str(button.get_id())][0]
-                                        button.set_price(round(button.get_price() * 1.15))
+                            if button[0].rect.collidepoint(event.pos):
+                                if self.money >= button[0].get_price():
+                                    self.money -= button[0].get_price()
+                                    if ID_UPGRADE_COST[str(button[0].get_id())][1] == "INC":
+                                        self.base_income += ID_UPGRADE_COST[str(button[0].get_id())][0]
+                                        button[0].set_price(round(button[0].get_price() * 1.35))
+                                    else:
+                                        self.multiplier *= ID_UPGRADE_COST[str(button[0].get_id())][0]
+                                        button[0].set_price(round(button[0].get_price() * 2.95))
+
 
                 if event.type == self.balance_trigger:
                     self.balance = self.font.render(f"Баланс: {self.money}", True, (255, 255, 255))
                     self.earn = self.font_income.render(f"Вы получите: {self.base_income * self.multiplier * self.rebirth_multiplier}", True, (255, 255, 255))
-
             pygame.display.flip()
 
 
